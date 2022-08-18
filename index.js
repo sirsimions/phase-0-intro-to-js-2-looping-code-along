d03c2ba532a2ebb18dd2af11ab7592558471f74b
Simion <simion.okola@student.moringaschool.com>
2022-08-19 02:55:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(cards) {
+    var message = [];
+    for (let i = 0; i < cards.length; i++ ){
+            message[i] = `Thank you, ${cards[i]}, for the wonderful surprise gift!`;
+        }
+        return message;
+}
+writeCards(cards);  
+
+function countDown(k){
+    for (let input = k; input >= 0; input--) {
+        console.log(input);
+       }
+}  
+countDown(10);
